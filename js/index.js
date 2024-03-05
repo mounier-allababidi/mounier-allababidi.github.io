0e4ffb595b01853f15c7b8a3291cbcedabcef905
mounier-allababidi <161966325+mounier-allababidi@users.noreply.github.com>
2024-03-05 23:39:33 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,4 +6,34 @@ $(document).ready(function () {
     $(".navbar-nav").find(".active").removeClass("active");
     $(this).addClass("active");
   });
+
+  $('#video_1').on('play', function() {
+    // Pause videoB if it is playing
+    if (!$('#video_2')[0].paused) {
+      $('#video_2')[0].pause();
+    }
+    if (!$('#video_3')[0].paused) {
+      $('#video_3')[0].pause();
+    }
+  });
+
+  $('#video_2').on('play', function() {
+    // Pause videoB if it is playing
+    if (!$('#video_3')[0].paused) {
+      $('#video_3')[0].pause();
+    }
+    if (!$('#video_1')[0].paused) {
+      $('#video_1')[0].pause();
+    }
+  });
+
+  $('#video_3').on('play', function() {
+    // Pause videoB if it is playing
+    if (!$('#video_2')[0].paused) {
+      $('#video_2')[0].pause();
+    }
+    if (!$('#video_1')[0].paused) {
+      $('#video_1')[0].pause();
+    }
+  });
 });
